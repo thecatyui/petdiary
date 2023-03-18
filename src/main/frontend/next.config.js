268eb5ac5471ac3e26b8bccfaf2fd6e9f43e7334
thecatyui <890511lkh@gmail.com>
2023-03-19 02:57:10 +0900

    Create Frontend

---
+++ src/main/frontend/next.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*' // Proxy to Backend
+      }
+    ]
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg/,
+      use: {
+        loader: 'svg-url-loader',
+      },
+    });
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/i,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash].[ext]',
+          publicPath: '/_next/static',
+          outputPath: 'static',
+          emitFile: !options.isServer,
+        },
+      },
+    });
+    return config;
+  },
+}
+
+module.exports = nextConfig;
