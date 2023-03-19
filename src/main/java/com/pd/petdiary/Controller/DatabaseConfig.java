8307a1ca111deb58df90824b3e00dfcf9e6a9812
thecatyui <890511lkh@gmail.com>
2023-03-19 15:01:28 +0900

    [backend]: database config

---
+++ src/main/java/com/pd/petdiary/Controller/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.pd.petdiary.Controller;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+}
